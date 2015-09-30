3b33c3b263e5bfa4dcc6e4e20daa709b4f556863
Luiz Gomes <luiz.fs.gomes@gmail.com>
2015-09-30 21:00:18 +0000

    invert string added

--- Lab3/PalindromeExercise.py
+++ Lab3/PalindromeExercise.py
@@ -1,2 +1,5 @@
 # Asks the user for input of the word and makes it lower case.
 normStr = raw_input("Enter the word:\n").lower();
+# Inverts the string so it can compare it with the original input.
+invertStr = normStr[::-1];
+
