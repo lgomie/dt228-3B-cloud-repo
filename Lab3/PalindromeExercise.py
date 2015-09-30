c4903f5b631bba21e17be1b7deb118c0c9571432
Luiz Gomes <luiz.fs.gomes@gmail.com>
2015-09-30 21:10:44 +0000

    Test added. Program should be complete.

--- Lab3/PalindromeExercise.py
+++ Lab3/PalindromeExercise.py
@@ -3,3 +3,8 @@ normStr = raw_input("Enter the word:\n").lower();
 # Inverts the string so it can compare it with the original input.
 invertStr = normStr[::-1];
 
+# Tests if the string is a palindrome. If so, it prints True. Else, prints False.
+if normStr == invertStr:
+  print 'True';
+else:
+  print 'False';
