485e4d16a300ef5eab54607d24f481886bf8ad3a
Luiz Gomes <luiz.fs.gomes@gmail.com>
2015-09-30 22:04:17 +0000

    Hopefully last commit on this one.

--- Lab3/PalindromeExercise.py
+++ Lab3/PalindromeExercise.py
@@ -1,9 +1,7 @@
 # Asks the user for input of the word and makes it lower case.
 normStr = raw_input("Enter the word:\n").lower();
-# Inverts the string so it can compare it with the original input.
 
-
-# Tests if the string is a palindrome. If so, it prints True. Else, prints False.
+# Tests if the string is a palindrome, inverting the input. If so, it prints True. Else, prints False.
 if normStr == normStr[::-1]:
   print 'True';
 else:
